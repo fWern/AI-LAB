ebbc097125c39d3925ef73484a4618a23f5bac14
Felix Wernlein <wernleinfelix@t-online.de>
2024-02-20 13:35:22 +0100

    added example as command

--- example.py
+++ example.py
@@ -4,11 +4,16 @@ from main import link_entity_to_text
 QUERY = "this is a sample text for the example to add an entity, in this case machine learning, to an text."
 
 # output of the function are the found entities and the text linked with entities
+# (first position in entry is ORKG entity, second position is part of text, for which the entity was found
 entities, text_linked = link_entity_to_text(QUERY)
+#Entities found for the QUERY :
+#[
+# ['http://orkg.org/orkg/resource/R4322', 'entity, in this case machine learning, to an text'],
+# ['http://orkg.org/orkg/resource/R4647', 'sample text for the example']
+# ]
 
 print(f"Input to Falcon 2.0: \n{QUERY}\n")
-
-# (first position in entry is ORKG entity, second position is part of text, for which the entity was found
 print(f"Entities found for the QUERY : \n{entities}\n")
-
+# Output text with linked entities:
+# this is a \uri{http://orkg.org/orkg/resource/R4647}{sample text for the example} to add an \uri{http://orkg.org/orkg/resource/R4322}{entity, in this case machine learning, to an text}.
 print(f"Output text with linked entities: \n{text_linked}")
